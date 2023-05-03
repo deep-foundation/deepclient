7df22d0245a24964e6aba64d6dc78197fde7f5b1
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-03 19:23:33 +0000

    0.0.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deep_client",
-    version="0.0.1", 
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         # Add your package dependencies here
