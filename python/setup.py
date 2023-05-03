37762f36f4f7f93cc093921d65ade67c703c5990
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-03 19:04:11 +0000

    Autopublish

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deep_client",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    description="Deep Client - a way to connect your favorite language with Deep",
+    license="Unlicense",
+    url="https://github.com/deep-foundation/deep-client",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
