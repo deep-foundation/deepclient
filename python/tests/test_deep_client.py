64e9328c1285a09a1523626315ecbf8c2d26804a
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-05 09:57:13 +0000

    tests are updated.

--- python/tests/test_deep_client.py
+++ python/tests/test_deep_client.py
@@ -35,24 +35,94 @@ class TestDeepClient(unittest.TestCase):
             with self.assertRaises(NotImplementedError):
                 method()
 
+    def test_serialize_where(self):
+        assert self.client.serialize_where({"id": 5}) == {"id": {"_eq": 5}}
+        assert self.client.serialize_where({"id": {"_eq": 5}}) == {"id": {"_eq": 5}}
+        assert self.client.serialize_where({"value": 5}) == {"number": {"value": {"_eq": 5}}}
+        assert self.client.serialize_where({"value": "a"}) == {"string": {"value": {"_eq": "a"}}}
+        assert self.client.serialize_where({"number": 5}) == {"number": {"value": {"_eq": 5}}}
+        assert self.client.serialize_where({"string": "a"}) == {"string": {"value": {"_eq": "a"}}}
+        assert self.client.serialize_where({"number": {"value": {"_eq": 5}}}) == {"number": {"value": {"_eq": 5}}}
+        assert self.client.serialize_where({"string": {"value": {"_eq": "a"}}}) == {"string": {"value": {"_eq": "a"}}}
+        assert self.client.serialize_where({"object": {"value": {"_contains": {"a": "b"}}}}) == {"object": {"value": {"_contains": {"a": "b"}}}}
 
-    def test_serializeWhere(self):
-        query = {
-            "from_id": {"_type_of": "Package"},
-            "_and": [
-                {"to_id": {"_type_of": "Selector"}},
-                {"type_id": {"_type_of": "Contain"}}
-            ]
+        # # Note: Add `async` and `await` for the below test case when implementing in the actual test file
+        # type_id_contain = self.client.id("@deep-foundation/core", "Contain")
+        # type_id_package = self.client.id("@deep-foundation/core", "Package")
+        assert self.client.serialize_where(
+            {
+                "out": {
+                    "type_id": 3,
+                    "value": "b",
+                    "from": {
+                        "type_id": 2,
+                        "value": "a",
+                    },
+                },
+            }
+        ) == {
+            "out": {
+                "type_id": {"_eq": 3},
+                "string": {"value": {"_eq": "b"}},
+                "from": {
+                    "type_id": {"_eq": 2},
+                    "string": {"value": {"_eq": "a"}},
+                },
+            }
         }
-        expected_result = {
-            "from_id": {"_by_item": {"path_item_id": {"_eq": "Package"}, "group_id": {"_eq": 0}}},
-            "_and": [
-                {"to_id": {"_by_item": {"path_item_id": {"_eq": "Selector"}, "group_id": {"_eq": 0}}}},
-                {"type_id": {"_by_item": {"path_item_id": {"_eq": "Contain"}, "group_id": {"_eq": 0}}}}
-            ]
+
+        assert self.client.serialize_where({"value": 5, "link": {"type_id": 7}}, "value") == {
+            "value": {"_eq": 5},
+            "link": {
+                "type_id": {"_eq": 7}
+            },
         }
-        serialized_query = self.client.serialize_where(query)
-        self.assertEqual(serialized_query, expected_result)
+
+        assert self.client.serialize_where({"type": ["@deep-foundation/core", "Value"]}) == {
+            "type": {
+                "in": {
+                    "from": {
+                        "string": {"value": {"_eq": "@deep-foundation/core"}},
+                        "type_id": {"_eq": 2},
+                    },
+                    "string": {"value": {"_eq": "Value"}},
+                    "type_id": {"_eq": 3},
+                },
+            },
+        }
+
+        assert self.client.serialize_where({"_or": [{"type": ["@deep-foundation/core", "Value"]}, {"type": ["@deep-foundation/core", "User"]}]}) == {
+            "_or": [{
+                "type": {
+                    "in": {
+                        "from": {
+                            "string": {"value": {"_eq": "@deep-foundation/core"}},
+                            "type_id": {"_eq": 2},
+                        },
+                        "string": {"value": {"_eq": "Value"}},
+                        "type_id": {"_eq": 3},
+                    },
+                },
+            }, {
+                "type": {
+                    "in": {
+                        "from": {
+                            "string": {"value": {"_eq": "@deep-foundation/core"}},
+                            "type_id": {"_eq": 2},
+                        },
+                        "string": {"value": {"_eq": "User"}},
+                        "type_id": {"_eq": 3},
+                    },
+                },
+            }]
+        }
+
+        # id_value = self.client.id("@deep-foundation/core", "Value")
+        # assert id_value == 4
+
+        assert self.client.serialize_where({"type_id": {"_type_of": 25}}) == {"type": {"_by_item": {"path_item_id": {"_eq": 25}, "group_id": {"_eq": 0}}}}
+        assert self.client.serialize_where({"from_id": {"_type_of": 25}}) == {"from": {"_by_item": {"path_item_id": {"_eq": 25}, "group_id": {"_eq": 0}}}}
+        assert self.client.serialize_where({"to_id": {"_type_of": 25}}) == {"to": {"_by_item": {"path_item_id": {"_eq": 25}, "group_id": {"_eq": 0}}}}
 
 
 if __name__ == '__main__':
