19471191f3f3d39b5c8fc21eafcd51ac19aed3ff
AutoPR <autopr@irgolic.com>
2023-05-07 15:43:59 +0000

    Fix NoneType mapping issue in select method of DeepClient

---
+++ deep_client.py
@@ -0,0 +1,20 @@
+def select(self, index: int) -> None:
+    if self.variables is None:
+        raise RuntimeError("Variables mapping is not initialized. Make sure to call 'connect' method first.")
+
+    try:
+        await self._send_query(
+            operation_name="Select",
+            query="""
+                mutation Select($index: Int!) {
+                    select(index: $index) {
+                        index
+                    }
+                }
+            """,
+            variables={
+                "index": index
+            },
+        )
+    except Exception as e:
+        raise RuntimeError(f"Error executing 'select' mutation: {e}")
